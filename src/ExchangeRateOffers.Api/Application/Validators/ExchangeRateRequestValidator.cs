7455892e4d4de49695ee49da06a0057d0123c5a4
Felix Jose Ynfante Quezada <fynfante@banreservas.com>
2025-07-28 05:46:31 -0400

    feat: add new validation to ExchangeRequest

--- src/ExchangeRateOffers.Api/Application/Validators/ExchangeRateRequestValidator.cs
+++ src/ExchangeRateOffers.Api/Application/Validators/ExchangeRateRequestValidator.cs
@@ -9,11 +9,16 @@ public class ExchangeRateRequestValidator : AbstractValidator<ExchangeRateReques
     {
         RuleFor(x => x.SourceCurrency)
             .NotEmpty().WithMessage("Source currency is required.")
-            .Length(3).WithMessage("Source currency must be a 3-letter code.");
+            .Length(3).WithMessage("Source currency must be a 3-letter code.")
+            .Matches("^[A-Z]{3}$").WithMessage("Target currency must be in uppercase (e.g., DOP).");
 
         RuleFor(x => x.TargetCurrency)
             .NotEmpty().WithMessage("Target currency is required.")
-            .Length(3).WithMessage("Target currency must be a 3-letter code.");
+            .Length(3).WithMessage("Target currency must be a 3-letter code.")
+            .Matches("^[A-Z]{3}$").WithMessage("Target currency must be in uppercase (e.g., DOP).")
+            //.NotEqual(x => x.SourceCurrency)
+            //    .WithMessage("Source and target currencies must be different.")
+            ;
 
         RuleFor(x => x.Amount)
             .GreaterThan(0).WithMessage("Amount must be greater than zero.");
